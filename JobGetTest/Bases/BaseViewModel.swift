705eb39925462a950eb5f90663e9369529efd8a7
Dan Precup <dan.precup@farmdrop.co.uk>
2022-01-13 10:28:19 +0200

    Cleanup base view model

--- JobGetTest/Bases/BaseViewModel.swift
+++ JobGetTest/Bases/BaseViewModel.swift
@@ -9,30 +9,11 @@ import Combine
 import Foundation
 
 class BaseViewModel: LoadingNotifier {
+    
+    /// The loading notifier
     let isLoading = CurrentValueSubject<Bool, Never>(false)
-
-    var errorMessage = PassthroughSubject<String?, Never>()
+    
+    /// The cancellable bag
     var bag = Set<AnyCancellable>()
-
-    func handlerCompletion(_ completion: Subscribers.Completion<Error>) {
-        if case .failure(let error) = completion {
-            errorMessage.send("Something went wrong, please try again later.")
-            Log.error("Publisher completion error: \(error.localizedDescription)", error)
-            isLoading.value = false
-        }
-    }
-    
-    func handlePublisher<T>(_ publisher: AnyPublisher<T, Error>, completion: @escaping (T) -> Void ) {
-        handlePublisherWithoutStoring(publisher, completion: completion)
-            .store(in: &bag)
-    }
-    
-    func handlePublisherWithoutStoring<T>(_ publisher: AnyPublisher<T, Error>, completion: @escaping (T) -> Void ) -> AnyCancellable {
-        return publisher
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { [weak self] comp in
-                self?.handlerCompletion(comp)
-            }, receiveValue: completion)
-    }
 }
 
